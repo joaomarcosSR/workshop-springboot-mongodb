e4d1743b1838c4dd477c24af59fca744c242c5a5
Joao M. S. Ribeiro <joao.ribeiro@engenharia.ufjf.br>
2020-06-13 21:49:21 -0300

    insert user with POST

--- src/main/java/com/joaoribeiro/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/joaoribeiro/workshopmongodb/resources/UserResource.java
@@ -1,15 +1,18 @@
 package com.joaoribeiro.workshopmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joaoribeiro.workshopmongodb.domain.User;
 import com.joaoribeiro.workshopmongodb.dto.UserDTO;
@@ -34,4 +37,24 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok(new UserDTO(obj));
 	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(new UserDTO(obj));
+	}
+	
+//	@DeleteMapping(value = "/{id}")
+//	public ResponseEntity<Void> delete (@PathVariable Long id){
+//		service.delete(id);
+//		return ResponseEntity.noContent().build();
+//	}
+//	
+//	@PutMapping(value = "/{id}")
+//	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+//		obj = service.update(id, obj);
+//		return ResponseEntity.ok(obj);
+//	}
 }
--- src/main/java/com/joaoribeiro/workshopmongodb/services/UserService.java
+++ src/main/java/com/joaoribeiro/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaoribeiro.workshopmongodb.domain.User;
+import com.joaoribeiro.workshopmongodb.dto.UserDTO;
 import com.joaoribeiro.workshopmongodb.repositiries.UserRepository;
 import com.joaoribeiro.workshopmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -15,13 +16,48 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found. Id " + id));
 	}
+
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
+//	public void delete(Long id) {
+//		try {
+//			repository.deleteById(id);
+//		} catch (EmptyResultDataAccessException e) {
+//			throw new ResourceNotFoundException(id);
+//		} catch (DataIntegrityViolationException e) {
+//			throw new DatabaseException(e.getMessage());
+//		}
+//	}
+//
+//	public User update(Long id, User obj) {
+//		try {
+//			User entity = repository.getOne(id);
+//			updateData(entity, obj);
+//			return repository.save(entity);
+//		} catch (EntityNotFoundException e) {
+//			throw new ResourceNotFoundException(id);
+//		}
+//	}
+//
+//	private void updateData(User entity, User obj) {
+//		entity.setName(obj.getName());
+//		entity.setEmail(obj.getEmail());
+//		entity.setPhone(obj.getPhone());
+//	}
+
 }
